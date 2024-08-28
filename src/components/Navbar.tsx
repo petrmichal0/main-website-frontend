8e16479986158f72506a1d1603128a9d48a9dd46
Petr Michal <petrmichal0@gmail.com>
2024-08-29 00:31:04 +0200

    add components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import logo from "../assets/kevinRushLogo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
