1ccd3d45d68d8761fd1f7b69ad91b508f3166afb
Petr Michal <petrmichal0@gmail.com>
2024-09-22 22:22:40 +0200

    ADD projects style

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"; // Import useState pro správu filtrování
+import { useState } from "react";
 import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
 import {
@@ -15,29 +15,25 @@ import {
   SiVite,
   SiMongodb,
 } from "react-icons/si";
-import { AiOutlineLink } from "react-icons/ai"; // Ikona pro demo
-import { BiBook } from "react-icons/bi"; // Ikona pro dokumentaci
+import { AiOutlineLink } from "react-icons/ai";
+import { BiBook, BiRightArrowAlt } from "react-icons/bi"; // Ikona pro dokumentaci
 
-// Definice typu pro technologie a další odkazy
 type TechnologyIcon = {
   icon: JSX.Element;
   color: string;
 };
 
-// Definice typu pro odkazy na GitHub, demo a dokumentaci
 type LinkIcon = {
   icon: JSX.Element;
   color: string;
   label: string;
-  url?: string; // URL pro GitHub, demo nebo dokumentaci
+  url?: string;
 };
 
-// Definice typu pro objekt s technologiemi
 type TechnologyIcons = {
   [key: string]: TechnologyIcon;
 };
 
-// Mapa technologií a jejich ikon a barev
 const technologyIcons: TechnologyIcons = {
   React: { icon: <FaReact />, color: "bg-cyan-500" },
   TypeScript: { icon: <SiTypescript />, color: "bg-blue-500" },
@@ -48,27 +44,23 @@ const technologyIcons: TechnologyIcons = {
   MongoDB: { icon: <SiMongodb />, color: "bg-green-600" },
   CSS: { icon: <FaCss3Alt />, color: "bg-blue-400" },
   HTML: { icon: <FaHtml5 />, color: "bg-orange-500" },
-  GitHub: { icon: <FaGithub />, color: "bg-gray-800" },
 };
 
-// Mapa odkazů na GitHub, demo a dokumentaci
 const linkIcons: Record<string, LinkIcon> = {
   GitHub: { icon: <FaGithub />, color: "bg-gray-900", label: "GitHub" },
-  Demo: { icon: <AiOutlineLink />, color: "bg-green-600", label: "Demo" },
+  Demo: { icon: <AiOutlineLink />, color: "bg-gray-900", label: "Demo" },
   Documentation: {
     icon: <BiBook />,
-    color: "bg-blue-500",
+    color: "bg-gray-900",
     label: "Documentation",
   },
 };
 
-// Definování seznamu technologií pro filtrování
 const filterOptions = ["All", "React", "Node.js", "Angular", "Vue.js"];
 
 function Projects() {
-  const [activeFilter, setActiveFilter] = useState("All"); // Stav pro sledování aktuálního filtru
+  const [activeFilter, setActiveFilter] = useState("All");
 
-  // Funkce pro filtrování projektů podle technologie
   const filteredProjects = PROJECTS.filter((project) =>
     activeFilter === "All" ? true : project.technologies.includes(activeFilter)
   );
@@ -101,72 +93,33 @@ function Projects() {
         ))}
       </div>
 
-      <div>
+      {/* Kontejner s projekty */}
+      <div className="flex flex-wrap justify-center gap-8">
         {filteredProjects.map((project, index) => (
-          <div
+          <motion.div
             key={index}
-            className="mb-8 flex flex-wrap justify-center text-center lg:text-left lg:justify-center"
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5 }}
+            className="w-[350px] bg-neutral-800 p-6 rounded-lg shadow-md relative flex flex-col justify-between"
           >
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 0.5 }}
-              className="w-full lg:w-1/4 flex justify-center"
-            >
+            {/* Obrázek projektu */}
+            <div className="relative">
               <img
                 src={project.image}
-                width={150}
-                height={150}
                 alt={project.title}
-                className="mb-6 rounded"
+                className="w-full h-48 object-cover rounded-lg mb-4"
               />
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 0.5 }}
-              className="w-full max-w-xl lg:w-3/4 "
-            >
-              <h6 className="mb-2 font-semibold">{project.title}</h6>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
-
-              {/* Technologie */}
-              <div className="flex flex-wrap gap-2 mb-4">
-                {project.technologies.map((tech, index) => (
-                  <div
-                    key={index}
-                    className={`flex items-center space-x-2 rounded-md px-2 py-1 text-white text-sm font-medium ${
-                      technologyIcons[tech]?.color || "bg-neutral-800"
-                    }`}
-                  >
-                    {technologyIcons[tech]?.icon || null}
-                    <span>{tech}</span>
-                  </div>
-                ))}
-              </div>
-
-              {/* Odkazy na GitHub, demo a dokumentaci */}
-              <div className="flex flex-wrap gap-2">
+              {/* Ikony GitHub, Demo, Dokumentace v pravém dolním rohu */}
+              <div className="absolute bottom-9 right-0 flex space-x-2 bg-black  p-2 transform translate-y-1/2">
                 {project.github && (
                   <a
                     href={project.github}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`flex items-center space-x-2 rounded-md px-2 py-1 text-white text-sm font-medium ${linkIcons.GitHub.color}`}
+                    className="text-white"
                   >
-                    {linkIcons.GitHub.icon}
-                    <span>{linkIcons.GitHub.label}</span>
-                  </a>
-                )}
-                {project.demo && (
-                  <a
-                    href={project.demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`flex items-center space-x-2 rounded-md px-2 py-1 text-white text-sm font-medium ${linkIcons.Demo.color}`}
-                  >
-                    {linkIcons.Demo.icon}
-                    <span>{linkIcons.Demo.label}</span>
+                    <FaGithub className="w-6 h-6" />
                   </a>
                 )}
                 {project.documentation && (
@@ -174,15 +127,45 @@ function Projects() {
                     href={project.documentation}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`flex items-center space-x-2 rounded-md px-2 py-1 text-white text-sm font-medium ${linkIcons.Documentation.color}`}
+                    className="text-white"
                   >
-                    {linkIcons.Documentation.icon}
-                    <span>{linkIcons.Documentation.label}</span>
+                    <BiBook className="w-6 h-6" />
+                  </a>
+                )}
+                {project.demo && (
+                  <a
+                    href={project.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white flex items-center space-x-1"
+                  >
+                    <span className="text-sm">Demo</span>
+                    <BiRightArrowAlt className="w-4 h-4" />
                   </a>
                 )}
               </div>
-            </motion.div>
-          </div>
+            </div>
+
+            {/* Název projektu a popis */}
+            <div className="flex-grow">
+              <h6 className="font-semibold text-lg mb-2">{project.title}</h6>
+              <p className="text-neutral-400 mb-4">{project.description}</p>
+            </div>
+
+            {/* Technologie vlevo */}
+            <div className="flex justify-start items-center space-x-2">
+              {project.technologies.map((tech, index) => (
+                <div
+                  key={index}
+                  className={`flex items-center justify-center rounded-md w-8 h-8 text-white text-sm font-medium ${
+                    technologyIcons[tech]?.color || "bg-neutral-800"
+                  }`}
+                >
+                  {technologyIcons[tech]?.icon || null}
+                </div>
+              ))}
+            </div>
+          </motion.div>
         ))}
       </div>
     </div>
