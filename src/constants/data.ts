b588db492b135e107eb766623df1a9c0d9574031
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-06 21:51:06 +0200

    Update data.ts

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -13,21 +13,21 @@ export const EXPERIENCES = [
     role: "Administrative Worker",
     company: "TJMac s.r.o.",
     description: `Managed and optimized the company's e-shop. Communicated with clients and solved technical requirements. Created materials for marketing purposes. Ensured smooth office operations and supported the team.`,
-    technologie: [],
+    technologies: [],
   },
   {
     year: "2021 - 2022",
     role: "Team Leader",
     company: "Bidli s.r.o.",
     description: `Led a team of specialists in the energy sector. Planned and monitored results. Coordinated projects and introduced technical innovations. Supported the team in solving technological challenges.`,
-    technologie: [],
+    technologies: [],
   },
   {
     year: "2013 - 2021",
     role: "Team Leader",
     company: "Twenty-one s.r.o.",
     description: `Grew from operator to Team Leader. Led a team and managed projects. Provided technical coordination of projects. Planned and tracked project progress. Estimated capacity and budgets. Collaborated across the company to implement processes. Provided feedback and mentoring to team members.`,
-    technologie: [],
+    technologies: [],
   },
 ];
 
