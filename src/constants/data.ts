239662c852df71ae4d7839726daaba305def818c
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-06 21:38:30 +0200

    Update data.ts

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -9,32 +9,22 @@ export const ABOUT_TEXT: string = `My name is Petr Michal, and I always liked so
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "2022 - Present",
+    role: "Administrative Worker",
+    company: "TJMac s.r.o.",
+    description: `Managed and optimized the company's e-shop. Communicated with clients and solved technical requirements. Created materials for marketing purposes. Ensured smooth office operations and supported the team.`,
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    role: "Team Leader",
+    company: "Bidli s.r.o.",
+    description: `Led a team of specialists in the energy sector. Planned and monitored results. Coordinated projects and introduced technical innovations. Supported the team in solving technological challenges.`,
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2013 - 2021",
+    role: "Team Leader",
+    company: "Twenty-one s.r.o.",
+    description: `Grew from operator to Team Leader. Led a team and managed projects. Provided technical coordination of projects. Planned and tracked project progress. Estimated capacity and budgets. Collaborated across the company to implement processes. Provided feedback and mentoring to team members.`,
   },
 ];
 
