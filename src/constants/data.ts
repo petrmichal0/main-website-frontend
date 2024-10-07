db6cfc564250899ae37f14200c46371b923671db
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-07 10:22:25 +0200

    Update data.ts

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,4 +1,4 @@
-import project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/worldmapv1.png";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
