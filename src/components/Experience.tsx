004e492ecd53a2051426e8228e3130d641b00fce
Petr Michal <petrmichal0@gmail.com>
2024-11-13 23:07:29 +0100

    refactor styles in Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -27,38 +27,27 @@ function Experience() {
         Experience
       </motion.h1>
 
-      <div>
-        {EXPERIENCES.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div {...textAnimationLeft} className="w-full lg:w-1/4">
-              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
-            </motion.div>
+      {EXPERIENCES.map((experience, index) => (
+        <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+          <motion.div {...textAnimationLeft} className="w-full lg:w-1/4">
+            <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+          </motion.div>
 
-            <motion.div
-              {...textAnimationRight}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold">
-                {experience.role} -{" "}
-                <span className="text-sm text-purple-100">
-                  {experience.company}
-                </span>
-              </h6>
+          <motion.div
+            {...textAnimationRight}
+            className="w-full max-w-xl lg:w-3/4"
+          >
+            <h6 className="mb-2 font-semibold">
+              {experience.role} <span> - </span>
+              <span className="text-sm text-purple-100">
+                {experience.company}
+              </span>
+            </h6>
 
-              <p className="mb-4 text-neutral-400">{experience.description}</p>
-
-              {experience.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr2 mb-2 rounded-full bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
-                >
-                  {tech}
-                </span>
-              ))}
-            </motion.div>
-          </div>
-        ))}
-      </div>
+            <p className="mb-4 text-neutral-400">{experience.description}</p>
+          </motion.div>
+        </div>
+      ))}
     </div>
   );
 }
