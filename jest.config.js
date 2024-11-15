554d56b3835a935b91ae7ecf033d57b3a3343071
Petr Michal <petrmichal0@gmail.com>
2024-11-15 23:38:16 +0100

    basic settings jest and React Testing library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
---
+++ src/tests/integration/App.integration.test.tsx
@@ -0,0 +1,4 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import App from "../../App";
